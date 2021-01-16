00c7ec73670800c4dfaf8a3fcd6d66067d0128b7
Jatinder Koranga <jatinderskk8@gmail.com>
2021-01-16 16:36:02 -0500

    completed

---
+++ js101_book_exercises/functions/ex5.js
@@ -0,0 +1,8 @@
+function scream(words) {
+  return words + '!!!!';
+}
+
+scream('Yipeee');
+
+// It will not log anything.
+// This is because log method has not been called/invoked anywhere in the code.
