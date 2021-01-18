8f155386d92960a5bd3b4a8d95da594ef5304a29
Jatinder Koranga <jatinderskk8@gmail.com>
2021-01-17 23:59:47 -0500

    completed

--- js101_book_exercises/functions/ex5.js
+++ js101_book_exercises/functions/ex5.js
@@ -3,6 +3,7 @@ function scream(words) {
 }
 
 scream('Yipeee');
+scream('Jatinder');
 
 // It will not log anything.
-// This is because log method has not been called/invoked anywhere in the code.
+// This is because log method has not been called/invoked anywhere in the code.//dasdsdas                                                                                 
