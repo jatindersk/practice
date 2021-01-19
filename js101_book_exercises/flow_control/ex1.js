43fa6f975ea58f7a005ba7fa12b40ee87cff37bd
Jatinder Koranga <jatinderskk8@gmail.com>
2021-01-19 15:30:30 -0500

    completed

--- js101_book_exercises/flow_control/ex1.js
+++ js101_book_exercises/flow_control/ex1.js
@@ -1,12 +1,12 @@
-false || (true && false); // 
-true || (1 + 2);
-(1 + 2) || true;
-true && (1 + 2);
-false && (1 + 2);
-(1 + 2) && true;
-(32 * 4) >= 129;
-false !== !true;
-true === 4;
-false === (847 === '847');
-false === (847 == '847');
-(!true || (!(100 / 5) === 20) || ((328 / 4) === 82)) || false;
+false || (true && false); // false
+true || (1 + 2); // true
+(1 + 2) || true; // 3
+true && (1 + 2); // 3
+false && (1 + 2); // false
+(1 + 2) && true; // true
+(32 * 4) >= 129; // false
+false !== !true; // false
+true === 4; // false
+false === (847 === '847'); // true
+false === (847 == '847');  // false
+(!true || (!(100 / 5) === 20) || ((328 / 4) === 82)) || false; // true
