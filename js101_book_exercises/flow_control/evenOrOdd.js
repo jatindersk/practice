755da3f559264f9d11cee82890d10cb057e636e0
Jatinder Koranga <jatinderskk8@gmail.com>
2021-01-20 14:18:47 -0500

    completed

---
+++ js101_book_exercises/flow_control/evenOrOdd.js
@@ -0,0 +1,9 @@
+let evenOrOdd = num => {
+  if (num % 2 === 0) {
+    console.log('even');
+  } else console.log('odd');
+  (num % 2 === 0) ? console.log('even') : console.log('odd');
+};
+
+evenOrOdd(46);
+evenOrOdd(77);
