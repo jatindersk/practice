89841ee44a3174b44706500c37ad17712810c38f
Jatinder Koranga <jatinderskk8@gmail.com>
2021-01-22 11:42:42 -0500

    completed

--- js101_book_exercises/flow_control/ex7.js
+++ js101_book_exercises/flow_control/ex7.js
@@ -1,7 +1,13 @@
+// let uppercased = str => {
+//   if (str.length > 10) {
+//     return str.toUpperCase();
+//   } else return str;
+// };
+
+// using ternary operator
+
 let uppercased = str => {
-  if (str.length > 10) {
-    return str.toUpperCase();
-  } else return str;
+  return (str.length > 10) ? str.toUpperCase() : str;
 };
 
 console.log(uppercased('hello world'));
