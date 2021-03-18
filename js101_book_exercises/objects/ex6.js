554f55a036382151cc98bd57bd6af88cf4570c29
Jatinder Koranga <jatinderskk8@gmail.com>
2021-03-18 18:15:02 -0400

    completed w/o errors after re-reading the concerned material

---
+++ js101_book_exercises/objects/ex6.js
@@ -0,0 +1,8 @@
+    "foo" // primitive value
+    3.1415 // primitive value
+    [ 'a', 'b', 'c' ] // object
+    false // primitive value
+    foo // neither and object nor a primitive value
+    function bar() { return "bar"; } // object
+    undefined // primitive value
+    { a: 1, b: 2 } // object
