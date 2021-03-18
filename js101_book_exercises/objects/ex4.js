ef82fe8855d46e39d0b0f9b94a0ae848457552ce
Jatinder Koranga <jatinderskk8@gmail.com>
2021-03-18 17:45:34 -0400

    redone after a long break

--- js101_book_exercises/objects/ex4.js
+++ js101_book_exercises/objects/ex4.js
@@ -1,8 +1,17 @@
+// let obj = {
+//   b: 2,
+//   a: 1,
+//   c: 3,
+// };
+
+// let keysArr = Object.keys(obj);
+// console.log(keysArr.map(el => el.toUpperCase()));
+
 let obj = {
   b: 2,
   a: 1,
   c: 3,
 };
 
-let keysArr = Object.keys(obj);
-console.log(keysArr.map(el => el.toUpperCase()));
+let arr = Object.keys(obj).map(el => el.toUpperCase());
+console.log(arr);
